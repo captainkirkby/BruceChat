41468e7cd8495861a1d0ed5bef589c8287767236
Dylan Kirkby <dylan.kirkby@gmail.com>
2013-06-08 14:52:40 +0200

    Added some test routes

--- bruceChat.js
+++ bruceChat.js
@@ -2,5 +2,23 @@ var express = require("express");
 
 var app = express();
 
+//routing
+app.all("*", function(req, res, next){
+    res.writeHead(200, {"Content-Type" : "text/plain"});
+    next();
+});
+
+app.get("/", function(req, res){
+    res.end("Home");
+})
+
+app.get("/about", function(req, res){
+    res.end("About");
+})
+
+app.get("*", function(req, res){
+    res.end("404!");
+})
+
 app.listen(8888)
 console.log("Server is up on port 8888");
