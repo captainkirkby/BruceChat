affccaf6deaf01519c26d633d23795450a37486c
Dylan Kirkby <dylan.kirkby@gmail.com>
2013-06-08 14:54:51 +0200

    Added logger middleware

--- bruceChat.js
+++ bruceChat.js
@@ -2,6 +2,9 @@ var express = require("express");
 
 var app = express();
 
+//middleware
+app.use(express.logger());
+
 //routing
 app.all("*", function(req, res, next){
     res.writeHead(200, {"Content-Type" : "text/plain"});
@@ -10,15 +13,15 @@ app.all("*", function(req, res, next){
 
 app.get("/", function(req, res){
     res.end("Home");
-})
+});
 
 app.get("/about", function(req, res){
     res.end("About");
-})
+});
 
 app.get("*", function(req, res){
     res.end("404!");
-})
+});
 
 app.listen(8888)
 console.log("Server is up on port 8888");
