a036c8fb660b50e24802195269e0ec72671a3524
Dylan Kirkby <dylan.kirkby@gmail.com>
2013-06-08 20:28:39 +0200

    Tested replacing contents of chat area

--- public/js/jqueryFunctions.js
+++ public/js/jqueryFunctions.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
+	//hides the chat area
 	$("#chatArea").click(function(){
 		$(this).hide();
 	});
+
+	//reloads the chat area
+	$("#reloadChat").click(function(){
+		$("#chatArea").replaceWith("<div id='chatArea'>Reloaded Chat</div>");
+	})
 });
